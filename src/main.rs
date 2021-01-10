530e25d54fd71cfbd59e72525865b281839c1a14
PointyFluff <60830984+PointyFluff@users.noreply.github.com>
2021-01-10 10:57:27 -0800

    basic structure in place

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use qrcode::QrCode;
+use image::Luma;
+use structopt::StructOpt;
+use std::time::SystemTime;
+
+// how should this app behave?
+// run as a daemon or one time
+// accept user command line input (gonna need z clap, yah).
+// output errors to stderr, or just panic.
+// output a unixtimestamp.png (svg? zoidberg?)
+// Image generation could include options for 
+// different borders / backgrounds
+// and perhaps text
+// maybe be systemd friendly. maybe.
+// maybe a cowsay option. but with a qr bubble? 
+// naw too big...just text
+// in daemon mode we are gonna have to keep track of the day.
+
+#[derive(StructOpt)]
+struct Options {
+    message: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::from_args();
+    let timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+    let file_name = timestamp.to_string() + ".png";
+    let code = QrCode::new(options.message).unwrap();
+    let image = code.render::<Luma<u8>>().build();
+    image.save(file_name).unwrap();
 }
